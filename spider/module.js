45379bde7b4d3acfd74c7fbc3f69b06d13dc0e7f
nicole0169 <wow730@gmail.com>
2016-03-20 15:58:53 +0800

    模块化封装各功能.

---
+++ spider/module.js
@@ -0,0 +1,55 @@
+/**
+ * Created by minmiao on 16/3/12.
+ *
+ * 尝试试用模块调用的方式组织之前的功能代码
+ */
+var onepage = require('./onepage');
+var readfile = require('./readfile');
+var checkredis = require('./redisio');
+var getrm = require('./getrm');
+var downtor = require('./reqpost');
+var async = require("async");
+
+//初始化参数为t66y.com的页码号
+var page_start = process.argv[2];
+var page_end = process.argv[3];
+
+//参数必填否则提示退出
+if (!page_start || !page_end || (page_end < page_start) || (page_start <= 0)) {
+    console.log('You must input the start page number and end page number.');
+    process.exit();
+}
+
+onepage.setPageStart(page_start);
+onepage.setPageEnd(page_end);
+onepage.initTarget();
+onepage.getTarget(function (filename) {
+    console.log('Target save to the ' + filename + ' done!');
+    //文件已经保存下一步准备处理文件
+    readfile.readTargetFile(filename, function (tl) {
+        console.log('Target file has loaded!');
+        console.dir(tl);
+        //文件已经读取下一步准备处理REDIS
+        checkredis.redisIsExists(tl, function (newtl) {
+            console.log('Filter Target Array: ');
+            console.dir(newtl);
+            //判断是否在REDIS有过存储返回没有存储的全部内容下一步准备获取下载地址
+            if (newtl.length > 0) {
+                getrm.getRMurlFromTarget(newtl, function (rmdownlist) {
+                    console.log('RMdown lists: ');
+                    console.dir(rmdownlist);
+                    if (rmdownlist.length > 0) {
+                        async.each(rmdownlist, function (item, callback) {
+                            downtor.getRMtorrent(item.down,item.title,function(torinfo){
+                                console.log(torinfo + ' download completed!');
+                                callback();
+                            })
+                        }, function (err) {
+                            console.log('All torrent download completed!');
+                        });
+                    }
+                })
+            }
+        });
+    });
+});
--- spider/redisio.js
+++ spider/redisio.js
@@ -1,45 +1,62 @@
 /**
  * Created by minmiao on 15/10/25.
+ *
+ * 这个模块的作用是从REDIS判断链接地址是否已经存在
+ * 如果不存在则添加
+ * 如果存在则不用再做处理
  */
+var async = require("async");
 var redis = require("redis");
 var md5 = require("md5");
 var client = redis.createClient("6379", "127.0.0.1");
 
 var rhash = "mywife";
-var rkey = process.argv[2];
-var rval = process.argv[3];
+//var rkey = process.argv[2];
+//var rval = process.argv[3];
 
-if (!rkey || !rval) {
-    console.log('You must input the title and url.');
-    process.exit();
-}
-var md5rkey = md5(rkey);
+/*if (!rkey || !rval) {
+ console.log('You must input the title and url.');
+ process.exit();
+ }*/
+
+//var md5rkey = md5(rkey);
 
 // Start redis client
 client.on("connect", function () {
     console.log("Redis client connected.");
 });
 
-client.hexists(rhash, md5rkey, function (err, reply) {
-    console.log("Is key " + md5rkey + " exists in Redis? " + reply);
-    if (!reply) {
-        var newKv = {};
-        newKv[md5rkey] = rval;
-        client.hmset(rhash, newKv, function (err, reply) {
-            if (!err) {
-                client.hgetall(rhash, function (err, reply) {
+function redisIsExists(tl, cb) {
+    var newJobArr = [];
+    async.each(tl, function (item, callback) {
+        var tmp_rkey = item['title'];
+        var tmp_rval = item['link'];
+        var tmp_md5rkey = md5(tmp_rkey);
+
+        client.hexists(rhash, tmp_md5rkey, function (err, reply) {
+            console.log("Is key " + tmp_md5rkey + " exists in Redis? " + reply);
+            if (!reply) {
+                var newKv = {};
+                newKv[tmp_md5rkey] = tmp_rval;
+                client.hmset(rhash, newKv, function (err, reply) {
                     if (!err) {
-                        console.dir(reply);
+                        newJobArr.push(item);
                         client.end();
+                        callback();
                     } else {
                         client.end();
+                        callback();
                     }
                 });
             } else {
                 client.end();
+                callback();
             }
         });
-    } else {
-        client.end();
-    }
-});
+
+    }, function (err) {
+        cb(newJobArr);
+    });
+}
+
+exports.redisIsExists = redisIsExists;
